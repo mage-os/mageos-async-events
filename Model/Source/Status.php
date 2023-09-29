e1c4b86fb192f824ad7229946d92296099edf7e0
Andreas von Studnitz <avs@integer-net.de>
2023-09-29 12:22:32 +0200

    Display the "status" column in the subscription grid as select instead of text

---
+++ Model/Source/Status.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace MageOS\AsyncEvents\Model\Source;
+
+use Magento\Framework\Data\OptionSourceInterface;
+
+class Status implements OptionSourceInterface
+{
+    public function toOptionArray()
+    {
+        return [
+            ['value' => 0, 'label' => __('Disabled')],
+            ['value' => 1, 'label' => __('Enabled')],
+        ];
+    }
+}
